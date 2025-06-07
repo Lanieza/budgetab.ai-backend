efc0f45a85c2b63a7b98f317131f8b0d053653cc
Lanieza <lblopez@up.edu.ph>
2025-06-07 23:27:30 +0800

    chore: add Render domain to ALLOWED_HOSTS

--- budget_project/settings.py
+++ budget_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-th)!o@t-((3*=xhtu$myrxa+=9y8nq@0sv_gjs$@^pep#*k=qn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '<your-local-ip>', '<custom-domain>']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '<your-local-ip>', '<custom-domain>', 'budgetab-ai-backend.onrender.com']
 
 # Application definition
 
